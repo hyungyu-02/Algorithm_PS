83ee8c417ea8dd59b3aa8963bfcba227443b3580
Hyungyu <yona20020209@gmail.com>
2023-12-12 22:12:47 +0900

    Create 1920_수_찾기.py

---
+++ search/1920_수_찾기.py
@@ -0,0 +1,11 @@
+#Binary Search
+n = int(input())
+A = set(map(int, input().split()))
+m = int(input())
+arr = list(map(int, input().split()))
+
+for i in arr:
+    if i in A:
+        print('1')
+    else:
+        print('0')
