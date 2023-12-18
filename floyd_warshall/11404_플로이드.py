0cc9653831bb2adbc638adaa222ff0deab08fcaa
Hyungyu <yona20020209@gmail.com>
2023-12-18 22:38:36 +0900

    Create 11404_플로이드.py

---
+++ floyd_warshall/11404_플로이드.py
@@ -0,0 +1,27 @@
+#플로이드 워셜, 그래프
+import sys
+input = sys.stdin.readline
+INF = 1e9
+n = int(input())
+m = int(input())
+
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
+            
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print('0', end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
