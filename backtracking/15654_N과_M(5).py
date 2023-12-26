0fc90b615354b72913a6eca73aaa456f1d9f2f71
Hyungyu <yona20020209@gmail.com>
2023-12-26 22:46:47 +0900

    Create 15654_N과_M(5).py

---
+++ backtracking/15654_N과_M(5).py
@@ -0,0 +1,19 @@
+#백트래킹
+n, m = map(int, input().split())
+
+source = list(map(int, input().split()))
+source.sort()
+
+grp = []
+
+def bt():
+    if len(grp) == m:
+        print(' '.join(map(str, grp)))
+        return
+    for i in source:
+        if i not in grp:
+            grp.append(i)
+            bt()
+            grp.pop()
+
+bt()
