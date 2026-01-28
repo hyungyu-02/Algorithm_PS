7da75c7cf9ab994d2f08d06836dc6168b68335b4
Hyungyu <yona20020209@gmail.com>
2026-01-28 11:50:23 +0900

    괄호 회전하기 문제

---
+++ stack/pg_괄호_회전하기.py
@@ -0,0 +1,24 @@
+def solution(s):
+    def correctBracket(a):
+        stack = []
+        for bracket in a:
+            if bracket == '(': stack.append(1)
+            elif bracket == '{': stack.append(2)
+            elif bracket == '[': stack.append(3)
+            elif bracket == ')':
+                if stack and stack[-1] == 1: stack.pop()
+                else: return False
+            elif bracket == '}':
+                if stack and stack[-1] == 2: stack.pop()
+                else: return False
+            elif bracket == ']':
+                if stack and stack[-1] == 3: stack.pop()
+                else: return False
+        return len(stack) == 0
+
+    answer = 0
+    for x in range(len(s)):
+        string = s[x:] + s[:x]
+        if correctBracket(string): answer += 1
+
+    return answer
