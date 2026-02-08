09ec755b22beab9b3808bfca8db2cbb480ee3219
Hyungyu <yona20020209@gmail.com>
2026-02-08 14:02:42 +0900

    프로그래머스 정수 삼각형 문제

---
+++ dynamic_programming/pg_정수_삼각형.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43105
+
+def solution(triangle):
+    
+    for i in range(len(triangle)-2, -1, -1):
+        for j in range(i + 1):
+            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
+    
+    return triangle[0][0]
