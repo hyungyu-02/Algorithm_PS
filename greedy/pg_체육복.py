52a75c56f4bdf93079ab54457fd76979f8102562
Hyungyu <yona20020209@gmail.com>
2026-02-15 14:29:47 +0900

    프로그래머스 체육복 문제

---
+++ greedy/pg_체육복.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42862
+
+def solution(n, lost, reserve):
+    _reserve = set(reserve) - set(lost)
+    _lost = set(lost) - set(reserve)
+    
+    for r in _reserve:
+        if r-1 in _lost:
+            _lost.remove(r-1)
+        elif r+1 in _lost:
+            _lost.remove(r+1)
+    
+    return n - len(_lost)
