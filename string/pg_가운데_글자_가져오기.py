d4c6a85e0f2d087899133fd7bbaff041d69b5eea
Hyungyu <yona20020209@gmail.com>
2026-02-20 23:49:06 +0900

    프로그래머스 가운데 글자 가져오기 문제

---
+++ string/pg_가운데_글자_가져오기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+def solution(s):
+    if len(s) % 2 == 0:
+        return s[len(s)//2 - 1: len(s)//2 + 1]
+    else:
+        return s[len(s)//2]
