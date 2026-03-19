8c2f2f55f2ef32e3766ade96f555a553a4253ff4
Hyungyu <yona20020209@gmail.com>
2026-03-20 00:54:08 +0900

    프로그래머스 레벨2 짝지어 제거하기 문제
    Add an alternative solution with a different approach to remove adjacent pairs from a string.

--- stack/짝지어_제거하기.py
+++ stack/짝지어_제거하기.py
@@ -10,3 +10,19 @@ def solution(s):
             stack.append(c)
     
     return 0 if stack else 1
+
+# 안좋은 풀이
+def solution(s):
+    pointer = 0
+    
+    while True:
+        if pointer > len(s) -2 or len(s) == 0 :
+            break
+        
+        if s[pointer] == s[pointer + 1]:
+            s = s[:pointer] + s[pointer + 2:] # 파이썬에서 문자열을 자르고 합칠 때마다 새로운 문자열 객체를 생성하며 O(N)의 시간이 걸림
+            pointer = 0 # 짝을 찾을 때마다 처음부터 다시 검사하기 때문에, 최악의 경우 시간 복잡도가 O(N^2)까지 치솟아 100만 개의 문자를 처리하기엔 너무 무거움
+        else:
+            pointer += 1
+        
+    return 1 if len(s) == 0 else 0
