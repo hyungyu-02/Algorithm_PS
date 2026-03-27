55eb15127419b0231fd531dcb65d4566d522f92e
Hyungyu <yona20020209@gmail.com>
2026-03-27 22:27:16 +0900

    프로그래머스 레벨2 뒤에 있는 큰 수 찾기 문제

---
+++ stack/뒤에_있는_큰_수_찾기.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+def solution(numbers):
+    answer = [-1 for _ in range(len(numbers))]
+    
+    stack = []
+    
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        
+        stack.append(i)
+                
+    return answer
